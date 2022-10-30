dde8b604c21eb1cf12d7aa15199017940e0f66fb
toby <tobyclh@gmail.com>
2022-10-31 06:33:07 +0900

    add open3d vis

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Flame PyTorch3D
+This code provides a quick visualization of the FLAME model. 
+
+
+
+## Installation
+You need to gather the flame data (FLAME 2020) from MPI. 
+
+
+
+
+## License
+This code is released under the MIT license. 
+The FLAME shape model is also released under a similar license. 
+
+However, the rest of the MPI resource (texture space, SMPL-X model and repo) are released under non-commerical only license. 
+Please respect their corresponding licenses when using this repo. 
---
+++ open3d_visualize.py
@@ -0,0 +1,74 @@
+
+# -*- coding: utf-8 -*-
+
+# Max-Planck-Gesellschaft zur Förderung der Wissenschaften e.V. (MPG) is
+# holder of all proprietary rights on this computer program.
+# You can only use this computer program if you have closed
+# a license agreement with MPG or you get the right to use the computer
+# program from someone who is authorized to grant you that right.
+# Any use of the computer program without a valid license is prohibited and
+# liable to prosecution.
+#
+# Copyright©2019 Max-Planck-Gesellschaft zur Förderung
+# der Wissenschaften e.V. (MPG). acting on behalf of its Max Planck Institute
+# for Intelligent Systems. All rights reserved.
+#
+# Contact: ps-license@tuebingen.mpg.de
+
+import os.path as osp
+import argparse
+import pickle
+
+import numpy as np
+import torch
+import open3d as o3d
+
+from smplx import FLAME
+
+
+def main(model_folder, ext='pkl',
+         gender='neutral'):
+    
+    model = FLAME(model_folder, gender=gender, ext=ext, num_betas=300, num_expression_coeffs=100)
+    betas = torch.zeros([1, 300], dtype=torch.float32)
+    expression = torch.zeros([1, 100], dtype=torch.float32)
+
+    output = model(betas=betas, expression=expression,
+                   return_verts=True)
+    vertices = output.vertices.detach().cpu().numpy().squeeze()
+    joints = output.joints.detach().cpu().numpy().squeeze()
+
+    print('Vertices shape =', vertices.shape)
+    print('Joints shape =', joints.shape)
+
+    mesh = o3d.geometry.TriangleMesh()
+    mesh.vertices = o3d.utility.Vector3dVector(vertices)
+    mesh.triangles = o3d.utility.Vector3iVector(model.faces)
+    mesh.compute_vertex_normals()
+
+    colors = np.ones_like(vertices) * [0.3, 0.3, 0.3]
+    # colors[head_idxs] = head_color
+
+    mesh.vertex_colors = o3d.utility.Vector3dVector(colors)
+
+    o3d.visualization.draw_geometries([mesh])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Flame ')
+
+    parser.add_argument('--model-folder', required=True, type=str,
+                        help='The path to the model folder')
+    parser.add_argument('--gender', type=str, default='generic',
+                        help='The gender of the model')
+    parser.add_argument('--ext', type=str, default='pkl',
+                        help='Which extension to use for loading')
+
+    args = parser.parse_args()
+
+    model_folder = osp.expanduser(osp.expandvars(args.model_folder))
+    gender = args.gender
+    ext = args.ext
+
+    main(model_folder, ext=ext,
+         gender=gender)
