1963af8b4b6a043ff90aa78dc53a3211ece3a80f
toby <tobyclh@gmail.com>
2022-11-01 01:26:02 +0900

    update interaction

--- pytorch_interactive.py
+++ pytorch_interactive.py
@@ -33,29 +33,39 @@ import os
 from smplx import FLAME
 import streamlit as st
 device = 'cuda'
-
+st.title('Flame model viewer')
 if '_is_init' not in st.session_state or not st.session_state['_is_init']:
+    st.session_state['_is_init'] = True
     model_folder = '/home/toby/Documents/FCS/Core/sample_data/FLAME2020/'
     gender = 'generic'
     ext = 'pkl'
-    st.session_state['shape'] = torch.zeros([1, 300], dtype=torch.float32)
-    st.session_state['exp'] = torch.zeros([1, 100], dtype=torch.float32)
+    for i in range(300):
+        st.session_state[f'shape_{i}'] = 0
+    for i in range(100):
+        st.session_state[f'exp_{i}'] = 0
     model = FLAME(model_folder, gender=gender, ext=ext, num_betas=300, num_expression_coeffs=100)
     st.session_state['flame_model'] = model
-    
-    output = model(betas=st.session_state['shape'], expression=st.session_state['exp'], return_verts=True)
+    np_shape = np.array([st.session_state[f'shape_{i}'] for i in range(300)], dtype=np.float32) 
+    np_exps = np.array([st.session_state[f'exp_{i}'] for i in range(100)], dtype=np.float32) 
+    betas = torch.tensor(np_shape, device='cpu', dtype=torch.float)[None]
+    exps = torch.tensor(np_exps, device='cpu', dtype=torch.float)[None]
+    output = model(betas=betas, expression=exps, return_verts=True)
     vertices = output.vertices.detach().to(device).squeeze()
     faces = torch.tensor(model.faces.astype(np.int32), dtype=torch.long, device=device)
     verts_rgb = torch.ones_like(vertices)[None] # (1, V, 3)
+    verts_rgb[..., 0] = 69/255
+    verts_rgb[..., 1] = 145/255
+    verts_rgb[..., 2] = 197/255
     textures = TexturesVertex(verts_rgb)
     st.session_state['textures'] = textures
     mesh = Meshes(verts=[vertices], faces=[faces], textures=textures)
     st.session_state['mesh'] = mesh
+    
     R, T = look_at_view_transform(0.6, 0, 0) 
     cameras = FoVPerspectiveCameras(device=device, R=R, T=T, fov=40)
 
     raster_settings = RasterizationSettings(
-        image_size=512, 
+        image_size=1024, 
         blur_radius=0.0, 
         faces_per_pixel=1, 
     )
@@ -73,16 +83,16 @@ if '_is_init' not in st.session_state or not st.session_state['_is_init']:
             lights=lights
         )
     )
-    st.set_page_config(layout="wide")
+    
     st.session_state['renderer'] = renderer
-    st.session_state['_is_init'] = True
+    st.set_page_config(layout="wide")
+    
+
 def change_color():
     color = st.session_state['color'].lstrip('#')
     color = tuple(int(color[i:i+2], 16) for i in (0, 2, 4))
     for i in range(3):
         st.session_state['textures']._verts_features_padded[..., i] = color[i]/255
-    # textures = TexturesVertex(verts_rgb)
-    # st.session_state['mesh'].textures = textures
 
 st.sidebar.color_picker('Vert color', value='#439fe0', on_change=change_color,  key='color')
 
@@ -91,19 +101,21 @@ col1, col2, col3 = st.columns([1, 1, 2])
 
 with col1.expander('Shape sliders'):
     start_index, end_index = col1.slider('Shape axis', min_value=0, max_value=299, value=[0, 10])
-    # start_index = st.number_input('start from', min_value=0, max_value=299-n_slider, value=10)
     for idx in range(start_index, end_index+1):
-        st.session_state['shape'][0, idx] = col1.slider(f'shape_{idx}', min_value=-2.0, max_value=2.0, value=0.0)
+        col1.slider(f'shape_{idx}', min_value=-2.0, max_value=2.0, value=0.0, key=f'shape_{idx}')
 
 with col2.expander('Exp sliders'):
     start_index, end_index = col2.slider('Exp axis', min_value=0, max_value=99, value=[0, 10])
-    # start_index = st.number_input('start from', min_value=0, max_value=299-n_slider, value=10)
     for idx in range(start_index, end_index+1):
-        st.session_state['exp'][0, idx] = col2.slider(f'exp_{idx}', min_value=-2.0, max_value=2.0, value=0.0)
+        col2.slider(f'exp_{idx}', min_value=-2.0, max_value=2.0, value=0.0, key=f'exp_{idx}')
 
 
 with torch.no_grad():
-    output = st.session_state['flame_model'](betas=st.session_state['shape'], expression=st.session_state['exp'], return_verts=True)
+    np_shape = np.array([st.session_state[f'shape_{i}'] for i in range(300)], dtype=np.float32) 
+    np_exps = np.array([st.session_state[f'exp_{i}'] for i in range(100)], dtype=np.float32) 
+    betas = torch.tensor(np_shape, device='cpu', dtype=torch.float)[None]
+    exps = torch.tensor(np_exps, device='cpu', dtype=torch.float)[None]
+    output = st.session_state['flame_model'](betas=betas, expression=exps, return_verts=True)
     st.session_state['mesh']._verts_padded = output.vertices.detach().to(device)
     images = st.session_state['renderer'](st.session_state['mesh'])
 fig, ax = plt.subplots()
