ed61a0db7e561dcf138ec7471dae0894e9f23cfe
toby <tobyclh@gmail.com>
2022-11-01 02:41:09 +0900

    add demo

--- Readme.md
+++ Readme.md
@@ -1,11 +1,35 @@
 # Flame PyTorch3D
 This code provides a quick visualization of the FLAME model. 
-
+    
+![](images/demo.gif)
 
 
 ## Installation
-You need to gather the flame data (FLAME 2020) from MPI. 
-You will also need PyTorch3D for basic render, and streamlit for interactive purpose. 
+You need to gather the flame data (FLAME 2020) from [MPI Flame webpage](https://flame.is.tue.mpg.de/download.php), login required.  
+Put the downloaded data under data/FLAME2020. 
+
+You will also need PyTorch3D for basic render, and streamlit for interactive purpose.   
+Consult [PyTorch3D](https://github.com/facebookresearch/pytorch3d/blob/main/INSTALL.md) installation page on how to install it.   
+streamlit can be installed with `pip install streamlit`.
+
+To view the mesh in 3D, you can use open3D with `pip install open3d`.
+
+
+## Running visualization/interaction
+- generate a single image from FLAME  
+  `python pytorch_visualize.py`.
+
+- view FLAME mesh in 3D  
+  `python open3d_visualize.py`
+
+- interactively FLAME parameters   
+  **CPU only** `streamlit run pytorch_interactive.py -- --img_size 256 --fov 40 --geneder generic --device cpu`  
+  **CUDA** `streamlit run pytorch_interactive.py -- --img_size 1024 --fov 40 --geneder generic --device cuda`  
+
+## Known Issues
+In the interactive app
+1. shading is not updated as the geometry changes.
+2. CPU only is rather slow
 
 
 
--- pytorch_interactive.py
+++ pytorch_interactive.py
@@ -1,23 +1,10 @@
-from pytorch3d.utils import ico_sphere
-from pytorch3d.io import load_obj
+from ast import parse
 from pytorch3d.structures import Meshes
-from pytorch3d.ops import sample_points_from_meshes
-from pytorch3d.loss import chamfer_distance
-import torch
 
-
-import os
 import torch
-import matplotlib.pyplot as plt
 import numpy as np
 
-# Util function for loading meshes
-from pytorch3d.io import load_objs_as_meshes, load_obj
-
-# Data structures and functions for rendering
 from pytorch3d.structures import Meshes
-from pytorch3d.vis.plotly_vis import AxisArgs, plot_batch_individually, plot_scene
-from pytorch3d.vis.texture_vis import texturesuv_image_matplotlib
 from pytorch3d.renderer import (
     look_at_view_transform,
     FoVPerspectiveCameras, 
@@ -29,27 +16,47 @@ from pytorch3d.renderer import (
     TexturesVertex
 )
 
-import os
 from smplx import FLAME
 import streamlit as st
-device = 'cuda'
-st.title('Flame model viewer')
+from argparse import ArgumentParser
+
+
 if '_is_init' not in st.session_state or not st.session_state['_is_init']:
+    st.set_page_config(layout="wide")
     st.session_state['_is_init'] = True
-    model_folder = '/home/toby/Documents/FCS/Core/sample_data/FLAME2020/'
-    gender = 'generic'
+    
+    parser = ArgumentParser('PyTorch3D interactive app for FLAME model')
+    parser.add_argument('--device', default='cuda')
+    parser.add_argument('--img_size', default=512, type=int, help='rendering resolution')
+    parser.add_argument('--fov', default=30, type=int, help='field of view for camera')
+    parser.add_argument('--gender', default='generic', type=str, choices=['generic', 'female', 'male'], help='gender of flame model')
+
+    args = parser.parse_args()
+    device = args.device
+    st.session_state['device'] = device
+
+    
+    model_folder = 'data/FLAME2020/'
+    gender = args.gender
     ext = 'pkl'
     for i in range(300):
         st.session_state[f'shape_{i}'] = 0
     for i in range(100):
         st.session_state[f'exp_{i}'] = 0
+    for i in range(3):
+        st.session_state[f'jaw_{i}'] = 0
+        st.session_state[f'neck_{i}'] = 0
     model = FLAME(model_folder, gender=gender, ext=ext, num_betas=300, num_expression_coeffs=100)
     st.session_state['flame_model'] = model
     np_shape = np.array([st.session_state[f'shape_{i}'] for i in range(300)], dtype=np.float32) 
     np_exps = np.array([st.session_state[f'exp_{i}'] for i in range(100)], dtype=np.float32) 
+    np_jaw = np.array([st.session_state[f'jaw_{i}'] for i in range(3)], dtype=np.float32) 
+    np_neck = np.array([st.session_state[f'neck_{i}'] for i in range(3)], dtype=np.float32) 
     betas = torch.tensor(np_shape, device='cpu', dtype=torch.float)[None]
     exps = torch.tensor(np_exps, device='cpu', dtype=torch.float)[None]
-    output = model(betas=betas, expression=exps, return_verts=True)
+    jaw = torch.tensor(np_jaw, device='cpu', dtype=torch.float)[None]
+    neck = torch.tensor(np_neck, device='cpu', dtype=torch.float)[None]
+    output = model(betas=betas, expression=exps, neck_pose=neck, jaw_pose=jaw, return_verts=True)
     vertices = output.vertices.detach().to(device).squeeze()
     faces = torch.tensor(model.faces.astype(np.int32), dtype=torch.long, device=device)
     verts_rgb = torch.ones_like(vertices)[None] # (1, V, 3)
@@ -62,10 +69,10 @@ if '_is_init' not in st.session_state or not st.session_state['_is_init']:
     st.session_state['mesh'] = mesh
     
     R, T = look_at_view_transform(0.6, 0, 0) 
-    cameras = FoVPerspectiveCameras(device=device, R=R, T=T, fov=40)
+    cameras = FoVPerspectiveCameras(device=device, R=R, T=T, fov=args.fov)
 
     raster_settings = RasterizationSettings(
-        image_size=1024, 
+        image_size=args.img_size, 
         blur_radius=0.0, 
         faces_per_pixel=1, 
     )
@@ -85,8 +92,8 @@ if '_is_init' not in st.session_state or not st.session_state['_is_init']:
     )
     
     st.session_state['renderer'] = renderer
-    st.set_page_config(layout="wide")
     
+st.title('Flame model viewer')
 
 def change_color():
     color = st.session_state['color'].lstrip('#')
@@ -98,28 +105,39 @@ st.sidebar.color_picker('Vert color', value='#439fe0', on_change=change_color,  
 
 col1, col2, col3 = st.columns([1, 1, 2])
 
+with col1.expander('Neck'):
+    for idx in range(3):
+        st.slider(f'neck_{idx}', min_value=-0.5, max_value=0.5, value=0.0, key=f'neck_{idx}')
+
+with col2.expander('Jaw'):
+    for idx in range(3):
+        st.slider(f'jaw_{idx}', min_value=-0.5, max_value=0.5, value=0.0, key=f'jaw_{idx}')
 
 with col1.expander('Shape sliders'):
-    start_index, end_index = col1.slider('Shape axis', min_value=0, max_value=299, value=[0, 10])
+    start_index, end_index = st.slider('Shape axis', min_value=0, max_value=299, value=[0, 3])
     for idx in range(start_index, end_index+1):
-        col1.slider(f'shape_{idx}', min_value=-2.0, max_value=2.0, value=0.0, key=f'shape_{idx}')
+        st.slider(f'shape_{idx}', min_value=-2.0, max_value=2.0, value=0.0, key=f'shape_{idx}')
 
 with col2.expander('Exp sliders'):
-    start_index, end_index = col2.slider('Exp axis', min_value=0, max_value=99, value=[0, 10])
+    start_index, end_index = st.slider('Exp axis', min_value=0, max_value=99, value=[0, 3])
     for idx in range(start_index, end_index+1):
-        col2.slider(f'exp_{idx}', min_value=-2.0, max_value=2.0, value=0.0, key=f'exp_{idx}')
-
+        st.slider(f'exp_{idx}', min_value=-2.0, max_value=2.0, value=0.0, key=f'exp_{idx}')
 
 with torch.no_grad():
     np_shape = np.array([st.session_state[f'shape_{i}'] for i in range(300)], dtype=np.float32) 
     np_exps = np.array([st.session_state[f'exp_{i}'] for i in range(100)], dtype=np.float32) 
+    np_jaw = np.array([st.session_state[f'jaw_{i}'] for i in range(3)], dtype=np.float32) 
+    np_neck = np.array([st.session_state[f'neck_{i}'] for i in range(3)], dtype=np.float32) 
     betas = torch.tensor(np_shape, device='cpu', dtype=torch.float)[None]
     exps = torch.tensor(np_exps, device='cpu', dtype=torch.float)[None]
-    output = st.session_state['flame_model'](betas=betas, expression=exps, return_verts=True)
-    st.session_state['mesh']._verts_padded = output.vertices.detach().to(device)
+    jaw = torch.tensor(np_jaw, device='cpu', dtype=torch.float)[None]
+    neck = torch.tensor(np_neck, device='cpu', dtype=torch.float)[None]
+    output = st.session_state['flame_model'](betas=betas, expression=exps, neck_pose=neck, jaw_pose=jaw, return_verts=True)
+    st.session_state['mesh']._verts_padded = output.vertices.detach().to(st.session_state['device'])
     images = st.session_state['renderer'](st.session_state['mesh'])
-fig, ax = plt.subplots()
-col3.image(images[0, ..., :3].cpu().numpy(), use_column_width=True)
+
+use_column_width = not st.sidebar.checkbox('original_size', value=False)
+col3.image(images[0, ..., :3].cpu().numpy(), use_column_width=use_column_width)
 # ax.imshow()
 # # plt.axis("off")
 # col3.pyplot(fig)
